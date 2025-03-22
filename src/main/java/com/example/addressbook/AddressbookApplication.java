bc7ea838451d1ddd3cda0a24e928dbdacfb3d9ba
prince356 <princechaurasia014@gmail.com>
2025-03-22 10:56:49 +0530

    code fixes

--- src/main/java/com/example/addressbook/AddressbookApplication.java
+++ src/main/java/com/example/addressbook/AddressbookApplication.java
@@ -2,8 +2,10 @@ package com.example.addressbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.example.addressbook.repository")
 public class AddressbookApplication {
 
 	public static void main(String[] args) {
