1348b80ee1264bd5dee28629c1b39a7676810dbf
prince356 <princechaurasia014@gmail.com>
2025-03-22 05:55:55 +0530

    UC2: Used Lombok for Getters and Setters in AddressBook model

--- src/main/java/com/example/addressbook/model/AddressBook.java
+++ src/main/java/com/example/addressbook/model/AddressBook.java
@@ -1,50 +1,15 @@
 package com.example.addressbook.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressBook {
     private int id;
     private String name;
     private String phoneNumber;
     private String email;
-
-    public AddressBook() {
-    }
-
-    public AddressBook(int id, String name, String phoneNumber, String email) {
-        this.id = id;
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
